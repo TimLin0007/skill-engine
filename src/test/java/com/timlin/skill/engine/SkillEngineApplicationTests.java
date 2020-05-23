b358ab3bafbfee403c5557248523a0414b7ab417
Tim.Lin <TimLin0007@gmail.com>
2020-05-24 00:20:47 +0800

    项目初始化

---
+++ src/test/java/com/timlin/skill/engine/SkillEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timlin.skill.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
